ced23b391890250eece695e5d8a6ddd1d8b46cb7
Coufal, Jan <jan.coufal@oracle.com>
2023-07-13 21:45:11 +0200

    initial commit

---
+++ src/main/java/cz/iddqd/imageleech/ImageLeechApplication.java
@@ -0,0 +1,13 @@
+package cz.iddqd.imageleech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageLeechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageLeechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/iddqd/imageleech/ImageLeechApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.iddqd.imageleech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageLeechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
