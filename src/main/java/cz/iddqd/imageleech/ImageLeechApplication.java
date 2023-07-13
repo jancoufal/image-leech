63d266980e8967efb3af52e31a20c7c3c6722508
Coufal, Jan <jan.coufal@oracle.com>
2023-07-13 23:50:36 +0200

    ImageLeechApplication.postConstruct()

--- src/main/java/cz/iddqd/imageleech/ImageLeechApplication.java
+++ src/main/java/cz/iddqd/imageleech/ImageLeechApplication.java
@@ -1,13 +1,23 @@
 package cz.iddqd.imageleech;
 
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ImageLeechApplication {
 
+	private static final Logger LOG = LoggerFactory.getLogger(ImageLeechApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(ImageLeechApplication.class, args);
 	}
 
+	@PostConstruct
+	public void postConstruct()
+	{
+		LOG.info("{} constructed.", ImageLeechApplication.class.getSimpleName());
+	}
 }
